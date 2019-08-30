46c8efe671030fee9b775aed8e92e9df5157f2a3
carlo <carlo.nicolini@iit.it>
2019-08-30 15:15:39 +0200

    ...

--- wilson2.py
+++ wilson2.py
@@ -26,7 +26,7 @@ class Wilson:
         
         # add the magic link from all nodes to n+1 with weight 1/q
         #self.H.add_weighted_edges_from([(self.nv,n, q) for n in G.nodes()])
-        self.H.add_weighted_edges_from([(n,self.nv, 1/q) for n in G.nodes()])
+        self.H.add_weighted_edges_from([(self.nv,n, q) for n in G.nodes()])
         #print(np.asarray([(u,v,w['weight']) for u,v,w in self.H.edges(data=True)]).reshape([60,3]))
 
     # Choose an edge from v's adjacency list (randomly)
@@ -99,42 +99,30 @@ def to_networkx(G, T, root_nodes=None):
         #print('|V|=%d |E|=%d' % (t.number_of_nodes(),t.number_of_edges()))
         nx.draw_networkx_edges(t, pos, width=2, edge_cmap=plt.cm.Set2, edge_color=[cmap(float(i)/n_trees)]*e )
     plt.axis('off')
-    #plt.show()
 
-# Controllare se beta è collegato alla massima distanza delle foglie dal nodo root
 if __name__=='__main__':
     reps = 10
-    G = nx.grid_2d_graph(20, 20, periodic=False)
+    G = nx.grid_2d_graph(5, 5, periodic=False)
     #G = nx.path_graph(100)
     G = nx.from_numpy_array(nx.to_numpy_array(G))
-    
     #G = nx.planted_partition_graph(4,20,1,0.01)
-    beta_range = np.logspace(-3, 3, 50)
+    beta_range = np.logspace(-2, 2, 20)
 
     L = nx.laplacian_matrix(G).toarray()
 
-    S = nq.entropy(L=L, beta_range=beta_range)
-    Tr = np.log([np.sum(np.exp(-beta*np.linalg.eigvalsh(L))) for beta in beta_range])
-
-
-    StreeQuenched = np.mean(np.log([[len(Wilson(G,q=q).sample()[1]) for _ in range(reps)] for q in beta_range]),1)
-    #StreeAnnealed = np.log(np.mean(([[len(Wilson(G,q=q).sample()[1]) for q in beta_range] for _ in range(reps)]),0))
-    plt.semilogx(beta_range, StreeQuenched, color='green', label='<log |R|>')
-    #plt.semilogx(beta_range, StreeAnnealed, color='purple', label='log (<|R|>)')
-    plt.semilogx(beta_range, np.log([Wilson(G,q=1/q).s() for q in beta_range]), color='magenta', label='stieltjes s(q)')
-    plt.semilogx(beta_range, S, color='blue', label='S(\\rho)')
-    #plt.semilogx(beta_range, Tr, color='red', label='Z')
+    plt.semilogx(beta_range, [np.mean([len(Wilson(G,beta).sample()[1]) for _ in range(reps)]) for beta in beta_range], label='E[|R|]')
+    plt.semilogx(beta_range, [Wilson(G,q=beta).s() for beta in beta_range], label='q Tr[(qI+L)^{-1}]' )
     plt.legend()
-    # plt.savefig('relation_entropy_random_forest.svg')
+    plt.grid(which='both')
     plt.show()
     
-    q = 10
-    for _ in range(1):
-        W = Wilson(G, q=q)
-        F,roots = W.sample()
-        print(W.s(), np.mean([len(Wilson(G, q=1/q).sample()[1]) for _ in range(reps)]) )
+    # q = 10
+    # for _ in range(1):
+    #     W = Wilson(G, q=q)
+    #     F,roots = W.sample()
+    #     print(W.s(), np.mean([len(Wilson(G, q=1/q).sample()[1]) for _ in range(reps)]) )
 
-        to_networkx(G, F, roots)
-        #plt.savefig('wilson_ring_of_cliques_5_10_beta_1E9.png')
-        #print([x for x in F.nodes() if F.degree(x)==1])
-        plt.show()
+    #     to_networkx(G, F, roots)
+    #     #plt.savefig('wilson_ring_of_cliques_5_10_beta_1E9.png')
+    #     #print([x for x in F.nodes() if F.degree(x)==1])
+    #     plt.show()
