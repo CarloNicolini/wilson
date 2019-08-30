ebcb3050714b90c481076dd393ef62c4170c49b3
linello <>
2019-08-31 00:11:14 +0200

    ...

--- wilson2.py
+++ wilson2.py
@@ -6,7 +6,9 @@ Wilson's algorithm for unweighted STs.
 import numpy as np
 import networkx as nx
 import sys
-sys.path.append('/home/carlo/workspace/networkqit')
+import os
+home = os.getenv('HOME')
+sys.path.append(home + '/workspace/networkqit')
 import matplotlib.pyplot as plt
 import networkqit as nq
 import random
@@ -19,14 +21,15 @@ class Wilson:
         self.H = nx.DiGraph()
         self.nv = G.number_of_nodes()
         self.q = q
+        self.L = nx.laplacian_matrix(self.G).toarray()
         
         # set edge attribute weight with weight 1
         self.H.add_weighted_edges_from([(u,v,1.0) for u,v in G.edges()])
         self.H.add_weighted_edges_from([(v,u,1.0) for u,v in G.edges()])
         
-        # add the magic link from all nodes to n+1 with weight 1/q
-        self.H.add_weighted_edges_from([(n,self.nv, q) for n in G.nodes()])
-        self.H.add_weighted_edges_from([(self.nv,n, q) for n in G.nodes()])
+        # add links from all nodes in the original graph to the root with weight q
+        self.root = self.nv
+        self.H.add_weighted_edges_from([(u,self.root, q) for u in G.nodes()])
 
     # Choose an edge from v's adjacency list (randomly)
     def random_successor(self, v):
@@ -36,7 +39,7 @@ class Wilson:
         return np.random.choice(nei, p = weight)
     
     def sample(self):
-        intree = [False] * (self.nv+1)
+        intree = [False] * self.H.number_of_nodes()
         successor = {}
         # put the additional node
         F = nx.DiGraph()
@@ -69,17 +72,15 @@ class Wilson:
                     F.add_edge(i,neighbor)
         
         if self.nv in self.roots:
-            self.roots.remove(self.nv)
-        # remove the magic node, together with all its links
-        F.remove_node(F.number_of_nodes() - 1)
-        #F.remove_edges_from([(u,self.nv,1.0) for u in F.nodes()])
+            self.roots.remove(self.root)
+        # remove the root node, together with all its links
+        F.remove_node(self.root)
         # save the leaves
-        self.leaves = [n for n in F.nodes() if F.degree(n)==1]
+        # self.leaves = [n for n in F.nodes() if F.degree(n)==1]
         return F, list(self.roots)
     
     def s(self):
-        L = nx.laplacian_matrix(self.G).toarray()
-        lambdai = np.linalg.eigvalsh(L)
+        lambdai = np.linalg.eigvalsh(self.L)
         return (self.q/(self.q + lambdai)).sum()
 
 def draw_sampling(G, T, root_nodes=None):
@@ -92,30 +93,24 @@ def draw_sampling(G, T, root_nodes=None):
     nx.draw_networkx_nodes(G, pos=pos, node_color='k', node_size=3, lines_width=0.1)
     nx.draw_networkx_edges(G, pos, edge_style='dashed', alpha=0.1, edge_color='k', edge_width=0.01)
 
-    cmap = matplotlib.cm.get_cmap('viridis')
+    cmap = matplotlib.cm.get_cmap('Set3')
     for i, t in enumerate(nx.connected_component_subgraphs(T)):
         e = nx.number_of_edges(t)
         #print('|V|=%d |E|=%d' % (t.number_of_nodes(),t.number_of_edges()))
         nx.draw_networkx_edges(t, pos, width=2, edge_cmap=plt.cm.Set2, edge_color=[cmap(float(i)/n_trees)]*e )
     plt.axis('off')
 
-def trace_estimator():
-    reps = 10
-    #G = nx.grid_2d_graph(5, 5, periodic=False)
-    #G = nx.path_graph(100)
-    #G = nx.from_numpy_array(nx.to_numpy_array(G))
-    G = nx.planted_partition_graph(4,20,1,0.01)
-    beta_range = np.logspace(-2, 2, 50)
+def trace_estimator(G):
+    reps = 1
+    beta_range = np.logspace(-2, 2, 100)
     L = nx.laplacian_matrix(G).toarray()
-    plt.semilogx(beta_range, [np.mean([len(Wilson(G,beta).sample()[1]) for _ in range(reps)]) for beta in beta_range], label='E[|R|]')
-    plt.semilogx(beta_range, [Wilson(G,q=beta).s() for beta in beta_range], label='q Tr[(qI+L)^{-1}]' )
+    plt.semilogx(beta_range, [np.mean([len(Wilson(G,1/beta).sample()[1]) for _ in range(reps)]) for beta in beta_range], label='E[|R|]')
+    plt.semilogx(beta_range, [Wilson(G,q=1/beta).s() for beta in beta_range], label='q Tr[(qI+L)^{-1}]' )
     plt.legend()
     plt.grid(which='both')
     plt.show()
 
-def sampling_example():
-    G = nx.grid_2d_graph(5, 5, periodic=False)
-    G = nx.from_numpy_array(nx.to_numpy_array(G))
+def sampling_example(G):
     q = 0.1    
     W = Wilson(G, q=q)
     F,roots = W.sample()
@@ -123,6 +118,9 @@ def sampling_example():
     plt.show()
 
 if __name__=='__main__':
-    trace_estimator()
-    sampling_example()
+    G = nx.grid_2d_graph(15, 15, periodic=False)
+    G = nx.from_numpy_array(nx.to_numpy_array(G))
+
+    trace_estimator(G)
+    sampling_example(G)
     
